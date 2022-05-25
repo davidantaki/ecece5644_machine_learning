c76b0e075f52891d142a341e045002780bdd3310
David Antaki <antakidavid@gmail.com>
2022-05-24 21:24:55 -0400

    Plotting ROC curve working

--- hw1/q1.py
+++ hw1/q1.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt  # For general plotting
 from matplotlib import cm
 import numpy as np
 from scipy.stats import multivariate_normal  # MVN not univariate
+from sklearn.metrics import confusion_matrix
 import prob_utils
 
 np.set_printoptions(suppress=True)
@@ -76,41 +77,142 @@ Nl = np.array([sum(sample_labels == l) for l in possible_labels])
 print("Number of samples from Class 0: {:d}, Class 1: {:d}".format(
     Nl[0], Nl[1]))
 
-# # Output samples and labels
-# X = np.zeros([num_samples, dimensions])
-# sample_labels = np.zeros(num_samples)
-# print("labels: {}".format(sample_labels))
+C = num_classes
 
-# possible_labels = np.array([0, 1])
+mu2 = np.array([[-0.5, 1, 1], [-0.5, 1, 1], [-0.5, 1, 1]])
+# Min prob. of error classifier
+# Conditional likelihoods of each class given x, shape (C, N)
+# Row 0 = likelihoods that given the sample, it is of Class 0.
+# Row 1 = likelihoods that given the sample, it is of Class 1.
+class_cond_likelihoods = np.array(
+    [multivariate_normal.pdf(np.transpose(X), mu2[c], Sigma[c]) for c in range(C)])
+print("class_cond_likelihoods:\n{}".format(class_cond_likelihoods))
 
-# # Decide randomly which samples will come from each component
-# u = np.random.rand(num_samples)
-# thresholds = np.cumsum(priors)
-# print("thresholds: {}".format(thresholds))
+# MAP classifier (is a special case of ERM corresponding to 0-1 loss)
+# 0-1 loss values yield MAP decision rule
+Lambda = np.ones((C, C)) - np.identity(C)
+print("Loss Matrix:\n{}".format(Lambda))
 
-# for c in range(num_classes):
-#     # Get randomly sampled indices for this component
-#     c_ind = np.argwhere(u <= thresholds[c])[:, 0]
-#     c_N = len(c_ind)  # No. of samples in this component
-#     sample_labels[c_ind] = c * np.ones(c_N)
-#     # Multiply by 1.1 to fail <= thresholds and thus not reuse samples
-#     u[c_ind] = 1.1 * np.ones(c_N)
-#     X[c_ind, :] = multivariate_normal.rvs(mu[c], Sigma[c], c_N)
+discriminant_score_erm = np.log(
+    class_cond_likelihoods[1]) - np.log(class_cond_likelihoods[0])
 
-# # Plot the original data and their true labels
-# plt.figure(figsize=(12, 10))
-# plt.plot(X[sample_labels == 0, 0],
-#          X[sample_labels == 0, 1], 'bo', label="Class 0")
-# plt.plot(X[sample_labels == 1, 0],
-#          X[sample_labels == 1, 1], 'rx', label="Class 1")
-# plt.legend()
-# plt.xlabel(r"$x_0$")
-# plt.ylabel(r"$x_1$")
-# plt.title("Data and True Labels")
-# plt.tight_layout()
-# plt.show()
+# Gamma threshold for MAP decision rule (remove Lambdas and you obtain same gamma on priors only; 0-1 loss simplification)
+gamma_map = (Lambda[1, 0] - Lambda[0, 0]) / \
+    (Lambda[0, 1] - Lambda[1, 1]) * priors[0]/priors[1]
+# Same as:
+# gamma_map = priors[0]/priors[1]
+print(gamma_map)
 
-# Actual number of samples generated from each class
-# Nl = np.array([sum(sample_labels == l) for l in possible_labels])
-# print("Number of samples from Class 0: {:d}, Class 1: {:d}".format(
-#     Nl[0], Nl[1]))
+decisions_map = discriminant_score_erm >= np.log(gamma_map)
+
+# Get indices and probability estimates of the four decision scenarios:
+# (true negative, false positive, false negative, true positive)
+
+# True Negative Probability
+ind_00_map = np.argwhere((decisions_map == 0) & (sample_labels == 0))
+p_00_map = len(ind_00_map) / Nl[0]
+# False Positive Probability
+ind_10_map = np.argwhere((decisions_map == 1) & (sample_labels == 0))
+p_10_map = len(ind_10_map) / Nl[0]
+# False Negative Probability
+ind_01_map = np.argwhere((decisions_map == 0) & (sample_labels == 1))
+p_01_map = len(ind_01_map) / Nl[1]
+# True Positive Probability
+ind_11_map = np.argwhere((decisions_map == 1) & (sample_labels == 1))
+p_11_map = len(ind_11_map) / Nl[1]
+
+# Probability of error for MAP classifier, empirically estimated
+prob_error_erm = np.array((p_10_map, p_01_map)).dot(Nl.T / num_samples)
+print(np.array((p_10_map, p_01_map)).shape)
+# Display MAP decisions
+fig = plt.figure(figsize=(10, 10))
+
+# class 0 circle, class 1 +, correct green, incorrect red
+print(X.shape)
+print(ind_00_map)
+X_transpose = np.transpose(X)
+plt.plot(X_transpose[ind_00_map, 0],
+         X_transpose[ind_00_map, 1], 'og', label="Correct Class 0")
+plt.plot(X_transpose[ind_10_map, 0],
+         X_transpose[ind_10_map, 1], 'or', label="Incorrect Class 0")
+plt.plot(X_transpose[ind_01_map, 0],
+         X_transpose[ind_01_map, 1], '+r', label="Incorrect Class 1")
+plt.plot(X_transpose[ind_11_map, 0],
+         X_transpose[ind_11_map, 1], '+g', label="Correct Class 1")
+
+plt.legend()
+plt.xlabel(r"$x_1$")
+plt.ylabel(r"$x_2$")
+plt.title("MAP Decisions (RED incorrect)")
+plt.tight_layout()
+plt.show()
+
+
+from sys import float_info # Threshold smallest positive floating value
+
+# Generate ROC curve samples
+def estimate_roc(discriminant_score, label):
+    Nlabels = np.array((sum(label == 0), sum(label == 1)))
+
+    sorted_score = sorted(discriminant_score)
+
+    # Use tau values that will account for every possible classification split
+    taus = ([sorted_score[0] - float_info.epsilon] + 
+             sorted_score +
+             [sorted_score[-1] + float_info.epsilon])
+
+    # Calculate the decision label for each observation for each gamma
+    decisions = [discriminant_score >= t for t in taus]
+
+    ind10 = [np.argwhere((d==1) & (label==0)) for d in decisions]
+    p10 = [len(inds)/Nlabels[0] for inds in ind10]
+    ind11 = [np.argwhere((d==1) & (label==1)) for d in decisions]
+    p11 = [len(inds)/Nlabels[1] for inds in ind11]
+
+    # ROC has FPR on the x-axis and TPR on the y-axis
+    roc = np.array((p10, p11))
+
+    return roc, taus
+
+# Construct the ROC for ERM by changing log(gamma)
+roc_erm, _ = estimate_roc(discriminant_score_erm, sample_labels)
+roc_map = np.array((p_10_map, p_11_map))
+
+fig_roc, ax_roc = plt.subplots(figsize=(10, 10))
+ax_roc.plot(roc_erm[0], roc_erm[1])
+ax_roc.plot(roc_map[0], roc_map[1], 'rx', label="Minimum P(Error) MAP", markersize=16)
+ax_roc.legend()
+ax_roc.set_xlabel(r"Probability of false alarm $P(D=1|L=0)$")
+ax_roc.set_ylabel(r"Probability of correct decision $P(D=1|L=1)$")
+
+
+plt.grid(True)
+plt.show()
+
+'''
+# Take diag so we have (C, C) shape of priors with prior prob along diagonal
+class_priors = np.diag(priors)
+print("class_priors:\n{}".format(class_priors))
+# class_priors*likelihood with diagonal matrix creates a matrix of posterior probabilities
+# with each class as a row and N columns for samples, e.g. row 1: [p(y1)p(x1|y1), ..., p(y1)p(xN|y1)]
+class_posteriors = class_priors.dot(class_cond_likelihoods)
+
+# MAP rule, take largest class posterior per example as your decisions matrix (N, 1)
+# Careful of indexing! Added np.ones(N) just for difference in starting from 0 in Python and labels={1,2,3}
+decisions = np.argmax(class_posteriors, axis=0)
+
+# Simply using sklearn confusion matrix
+print("Confusion Matrix (rows: Predicted class, columns: True class):")
+conf_mat = confusion_matrix(decisions, sample_labels)
+print(conf_mat)
+correct_class_samples = np.sum(np.diag(conf_mat))
+print("Total Number of Misclassified Samples: {:d}".format(
+    num_samples - correct_class_samples))
+
+# Alternatively work out probability error based on incorrect decisions per class
+# perror_per_class = np.array(((conf_mat[1,0]+conf_mat[2,0])/Nl[0], (conf_mat[0,1]+conf_mat[2,1])/Nl[1], (conf_mat[0,2]+conf_mat[1,2])/Nl[2]))
+# prob_error = perror_per_class.dot(Nl.T / N)
+
+prob_error = 1 - (correct_class_samples / num_samples)
+print("Empirically Estimated Probability of Error: {:.4f}".format(prob_error))
+'''
