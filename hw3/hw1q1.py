86d4e49a8f09e77771bd8169ff33270fd08cc2b3
David Antaki <antakidavid@gmail.com>
2022-06-10 14:43:52 -0400

    Add generation of data

--- hw3/hw1q1.py
+++ hw3/hw1q1.py
@@ -10,41 +10,54 @@ import matplotlib.colors as mcol
 import matplotlib.pyplot as plt  # For general plotting
 
 
-# Utility to visualize PyTorch network and shapes
-
 N = 100
+sigma0 = [[1, -0.5, 0.3],
+          [-0.5, 1, -0.5],
+          [0.3, -0.5, 1]]
+sigma1 = [[1, 0.3, -0.2],
+          [0.3, 1, 0.3],
+          [-0.2, 0.3, 1]]
+sigma2 = [[1, -0.5, 0.3],
+          [-0.5, 1, -0.5],
+          [0.3, -0.5, 1]]
+sigma3 = [[1, 0.3, -0.2],
+          [0.3, 1, 0.3],
+          [-0.2, 0.3, 1]]
 
 
-def create_data(N, noise=0.1):
-    # Uses the same covariance matrix, scaled identity, for all Gaussians
-    Sigma = noise * np.eye(2)
-    # Five gaussian means specified to span a square and its centre
+def create_data(N):
+    # 4 classes
     Gs = [
-        mvn(mean=[2, 2], cov=Sigma),
-        mvn(mean=[-2, -2], cov=Sigma),
-        mvn(mean=[2, -2], cov=Sigma),
-        mvn(mean=[-2, 2], cov=Sigma),
-        mvn(mean=[0, 0], cov=Sigma),
+        mvn(mean=[2, 2, 2], cov=sigma0),
+        mvn(mean=[-2, -2, -2], cov=sigma1),
+        mvn(mean=[2, -2, 2], cov=sigma2),
+        mvn(mean=[-2, 2, -2], cov=sigma3)
     ]
-    # Draw random variable samples and assign labels, note class 3 has less samples altogether
-    X = np.concatenate([G.rvs(size=N) for G in Gs])
-    y = np.concatenate((np.zeros(N), np.zeros(
-        N), np.ones(N), np.ones(N), 2 * np.ones(N)))
 
-    # Will return an X and y of shapes (5*N, 2) and (5*N)
-    # Representing our dataset of 2D samples
+    # Draw random variable samples and assign labels, note class 3 has less samples altogether
+    # G = each guassian distribution
+    X = np.concatenate([G.rvs(size=N) for G in Gs])
+    y = np.concatenate((np.zeros(N), np.ones(
+        N), 2*np.ones(N), 3*np.ones(N)))
+
+    # Will return an X of shape (4*N, 3dimensions)
+    # 4 classes each with N samples ^^
+    # y of shape (4*N) -> 1 label per sample
     return X, y
 
 
 X, y = create_data(N)
-C = len(np.unique(y))
+# Number of classes
+C = 4
 
+# Plot 2 features of the generated data.
 plt.figure(figsize=(10, 8))
 plt.plot(X[y == 0, 0], X[y == 0, 1], 'bx', label="Class 0")
 plt.plot(X[y == 1, 0], X[y == 1, 1], 'ko', label="Class 1")
 plt.plot(X[y == 2, 0], X[y == 2, 1], 'r*', label="Class 2")
-plt.xlabel(r"$x_1$")
-plt.ylabel(r"$x_2$")
+plt.plot(X[y == 3, 0], X[y == 3, 1], 'g^', label="Class 3")
+plt.xlabel(r"$x_0$")
+plt.ylabel(r"$x_1$")
 plt.title("Data and True Labels")
 plt.legend()
 plt.show()
