6632e561f2410d65c3bf41b311fb2689afb159f3
David Antaki <antakidavid@gmail.com>
2022-06-24 23:53:41 -0400

    Move into functions and training final model

--- hw4/hw4q2.py
+++ hw4/hw4q2.py
@@ -65,42 +65,48 @@ def generate_feature_vector(image):
     # Returns feature vector of normalized pixels as shape (height*width, 3 or 5)
     return image_np, normalized_data.T
 
-
-# Load image
-paraskier_color = imread(
-    'https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/segbench/BSDS300/html/images/plain/normal/color/60079.jpg')
+def read_in_image():
+    global paraskier_color
+    global paraskier_norm
+    global paraskier_np
+    # Load image
+    paraskier_color = imread(
+        'https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/segbench/BSDS300/html/images/plain/normal/color/60079.jpg')
+    # Get image feature vector=(row_index, col_index, R, G, B)
+    paraskier_np, paraskier_norm = generate_feature_vector(paraskier_color)
+    print(paraskier_norm.shape)
 
 # Show image
-# fig1, ax1 = plt.subplots(1, 1, figsize=(10, 10))
-# ax1.imshow(paraskier_color)
-# ax1.set_title("ParaSkier Color")
-# plt.show()
+def show_original_image():
+    fig1, ax1 = plt.subplots(1, 1, figsize=(10, 10))
+    ax1.imshow(paraskier_color)
+    ax1.set_title("ParaSkier Color")
+    plt.show()
 
-# Get image feature vector=(row_index, col_index, R, G, B)
-paraskier_np, paraskier_norm = generate_feature_vector(paraskier_color)
-print(paraskier_norm.shape)
+
 
 # Visualize the Components
-# fig2 = plt.figure(figsize=(10, 10))
-# ax2 = fig2.add_subplot(111, projection='3d')
-# ax2.scatter(paraskier_norm[:, 2], paraskier_norm[:, 3],
-#             paraskier_norm[:, 4], c='r')
-# ax2.set_xlabel("Red Component")
-# ax2.set_ylabel("Green Component")
-# ax2.set_zlabel("Blue Component")
-# ax2.set_title(
-#     "Visualizing the Image's RGB Components")
-# plt.show()
+def visualize_img_components():
+    fig2 = plt.figure(figsize=(10, 10))
+    ax2 = fig2.add_subplot(111, projection='3d')
+    ax2.scatter(paraskier_norm[:, 2], paraskier_norm[:, 3],
+                paraskier_norm[:, 4], c='r')
+    ax2.set_xlabel("Red Component")
+    ax2.set_ylabel("Green Component")
+    ax2.set_zlabel("Blue Component")
+    ax2.set_title(
+        "Visualizing the Image's RGB Components")
+    plt.show()
 
-# fig3, ax3 = plt.subplots(1, 1, figsize=(10, 10))
-# ax3.scatter(paraskier_norm[:, 2], paraskier_norm[:, 3])
-# ax3.set_title("ParaSkier Color")
-# ax3.set_xlabel("Red Component")
-# ax3.set_ylabel("Green Component")
-# plt.show()
+    fig3, ax3 = plt.subplots(1, 1, figsize=(10, 10))
+    ax3.scatter(paraskier_norm[:, 2], paraskier_norm[:, 3])
+    ax3.set_title("ParaSkier Color")
+    ax3.set_xlabel("Red Component")
+    ax3.set_ylabel("Green Component")
+    plt.show()
 
-cv_scores = []
-iteration_counter = 0
+# cv_scores = []
+# iteration_counter = 0
 # for n_components in range(1, 2):
 #     gmm = GaussianMixture(n_components=n_components, covariance_type="full")
 #     gmm.fit(paraskier_norm)
@@ -116,6 +122,9 @@ iteration_counter = 0
 
 
 def graph_log_likelihood_vs_n_components():
+    '''
+    For testing purposes.
+    '''
     scores = []
     for n_components in range(1, 100):
         gmm = GaussianMixture(n_components=n_components,
@@ -139,20 +148,22 @@ def get_optimal_number_of_gmm_components():
 
     # Cross Validation Params
     K = 2
-    max_num_components_to_try = 10
+    # max_num_components_to_try = 100
 
     # Partition data
     kf = KFold(n_splits=K, shuffle=True)
-    n_components_list = np.arange(1, max_num_components_to_try)
+    # n_components_list = np.arange(1, max_num_components_to_try)
+    n_components_list = [1,2,3,4,5,6,7,8,9,10]
 
     # Allocate space for CV
     scores = []
-    scores = np.empty((max(n_components_list), K))
+    # scores = np.empty((max(n_components_list), K))
 
     # CROSS VALIDATION
     for n_components in n_components_list:
         # K-fold cross validation
         k = 0
+        k_scores = []
         # NOTE that these subsets are of the TRAINING dataset
         for train_indices, valid_indices in kf.split(paraskier_norm):
             # Extract the training and validation sets from the K-fold split
@@ -166,12 +177,15 @@ def get_optimal_number_of_gmm_components():
 
             # Get log-likelihood on the validation set
             s = gmm.score(X_valid_k)
-            scores[n_components-1, k] = s
+            # scores[n_components-1, k] = s
+            k_scores.append(s)
             k += 1
 
             print(
                 "n_components: {}\tk-fold: {}\tLog-Likelihood: {}".format(n_components, k, s))
+        scores.append(k_scores)
 
+    scores = np.array(scores)
     print(scores)
     # Compute Average of scores
     scores = np.mean(scores, axis=1)
@@ -179,68 +193,61 @@ def get_optimal_number_of_gmm_components():
 
     # Choose optimal number of components.
     # Choose the number of components that maximizes the log-likelikhood.
-    # Stop optimizing when the log-likelihood stops increasing by more than 0.2
-    opt_threshold = 0.2
+    # Stop optimizing when the log-likelihood stops increasing by more than opt_threshold
+    opt_threshold = 0.1
     opt_s_i = -1
     for i in range(1, len(scores)):
         if scores[i]-scores[i-1] < opt_threshold:
             opt_s_i = i-1
             break
 
-
     # Plot Cross Validation results
     fig4, ax4 = plt.subplots(1, 1, figsize=(10, 10))
     ax4.set_ylabel("Log Likelihood")
     ax4.set_xlabel("# Components")
-    ax4.set_title("Log Likelihood vs # Components")
-    ax4.plot(n_components_list, scores)
-    ax4.plot(opt_s_i+1, scores[opt_s_i], marker='x', c='r')
+    ax4.set_title("Cross Validation Resutls: Log Likelihood vs # Components")
+    ax4.plot(n_components_list, scores, marker='.')
+    ax4.plot(opt_s_i+1, scores[opt_s_i], marker='x', c='r', label="Optimal # Components")
+    ax4.legend()
     plt.show()
 
     opt_n_component = opt_s_i+1
-    print(opt_n_component)
+    print("Optimal Num Components: {}".format(opt_n_component))
     return opt_n_component
 
-# def train_final_model(n_components):
-#     # TRAIN FINAL MODEL
 
-#     # PRINT SEGMENTED IMAGE
-#     gmm = GaussianMixture(n_components=opt_n_component,
-#                         covariance_type="full")
-#     gmm.fit(paraskier_norm)
-#     pixel_labels = gmm.predict(paraskier_norm)
-#     # print(np.unique(pixel_labels))
-#     # counts = pandas.Series(pixel_labels).value_counts()
-#     # for i in np.unique(pixel_labels):
-#     #     print(counts.get(i))
+def train_final_model(n_components):
+    # TRAIN FINAL MODEL
 
-#     fig4, ax4 = plt.subplots(1, 1, figsize=(10, 10))
-#     ax4.set_title("ParaSkier Segmented with {}-Components".format(2))
-#     labels_img = pixel_labels.reshape(paraskier_np.shape[0], paraskier_np.shape[1])
-#     ax4.imshow(labels_img)
-#     plt.show()
-#     '''
+    # PRINT SEGMENTED IMAGE
+    gmm = GaussianMixture(n_components=n_components,
+                          covariance_type="full")
+    gmm.fit(paraskier_norm)
+    pixel_labels = gmm.predict(paraskier_norm)
+    labels_img = pixel_labels.reshape(
+        paraskier_np.shape[0], paraskier_np.shape[1])
+    # print(np.unique(pixel_labels))
+    # counts = pandas.Series(pixel_labels).value_counts()
+    # for i in np.unique(pixel_labels):
+    #     print(counts.get(i))
 
-#     '''
-#     # Create figure to plot all K-means segmentation results for each example image
-#     fig, ax = plt.subplots(4, 4, figsize=(15, 15))
-#     ax[0, 0].imshow(bird_gray, cmap=plt.cm.gray)
-#     ax[0, 0].set_title("Bird Grey")
-#     ax[0, 0].set_axis_off()
-#     ax[1, 0].imshow(bird_color)
-#     ax[1, 0].set_title("Bird Color")
-#     ax[1, 0].set_axis_off()
-#     ax[2, 0].imshow(plane_gray, cmap=plt.cm.gray)
-#     ax[2, 0].set_title("Plane Grey")
-#     ax[2, 0].set_axis_off()
-#     ax[3, 0].imshow(plane_color)
-#     ax[3, 0].set_title("Plane Color")
-#     ax[3, 0].set_axis_off()
+    fig4, ax4 = plt.subplots(1, 2, figsize=(10, 10))
+    ax4[0].imshow(paraskier_color)
+    ax4[0].set_title("Original ParaSkier Color")
+    ax4[1].set_title("Clustering with {}-Components".format(n_components))
+    ax4[1].imshow(labels_img)
+    
 
-### K-means Algorithm ###
+    plt.show()
 
-plt.show()
+def main():
+    read_in_image()
+    # show_original_image()
+    # visualize_img_components()
+    # graph_log_likelihood_vs_n_components()
+    opt_n_components = get_optimal_number_of_gmm_components()
+    # train_final_model(2)
 
 
-# graph_log_likelihood_vs_n_components()
-get_optimal_number_of_gmm_components()
+if __name__ == '__main__':
+    main()
